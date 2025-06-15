df3155bb10918526dc539e2fd9b9efbb24dac08c
BBLKepling <90485361+BBLKepling@users.noreply.github.com>
2025-06-14 23:40:21 -0700

    1.5 pdb and 1.6 update

---
+++ 1.6/Source/Nailbunny/Properties/InternalDefOf.cs
@@ -0,0 +1,16 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Nailbunny
+{
+    [DefOf]
+    public static class InternalDefOf
+    {
+        static InternalDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(InternalDefOf));
+        }
+
+        public static ThingDef BBLK_Nailbunny;
+    }
+}
---
+++ 1.6/Source/Nailbunny/ThoughtWorker_Nailbunny_Soothe.cs
@@ -0,0 +1,28 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace Nailbunny
+{
+    public class ThoughtWorker_Nailbunny_Soothe : ThoughtWorker
+    {
+        private const float Radius = 5f;
+
+        protected override ThoughtState CurrentStateInternal(Pawn p)
+        {
+            if (!p.Spawned)
+            {
+                return false;
+            }
+            List<Thing> list = p.Map.listerThings.ThingsOfDef(InternalDefOf.BBLK_Nailbunny);
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (p.Position.InHorDistOf(list[i].Position, Radius))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
